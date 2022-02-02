008b7ab29e363c3fc312a1dd4eae5feaecbb91c5
odDevPwcGit <83954570+odDevPwcGit@users.noreply.github.com>
2022-02-02 16:18:07 +0530

    Update Others_usagecat_update_script.sql

--- Others_usagecat_update_script.sql
+++ Others_usagecat_update_script.sql
@@ -7,4 +7,10 @@ and isoldapplication=false and applicationstatus='CONNECTION_ACTIVATED')
 and date(to_timestamp(currentreadingdate/1000)) >= to_date('2021-09-09', 'yyyy-MM-dd');
 
 -- Update OTHERS to BPL
-update eg_ws_service set usagecategory='BPL' where usagecategory='OTHERS';
+update eg_ws_service set usagecategory='BPL' where connection_id in (
+select ews.connection_id
+from eg_ws_connection ewc
+	inner join eg_ws_service ews on ews.connection_id=ewc.id
+where ews.usagecategory='OTHERS'
+	and ewc.isoldapplication=false
+	and ewc.applicationstatus='CONNECTION_ACTIVATED');
