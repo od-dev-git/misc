a1f2cf043bb3b4db84d9347b36e39d5c1041996c
odDevPwcGit <83954570+odDevPwcGit@users.noreply.github.com>
2022-02-02 13:40:47 +0530

    Others usageCat update and meter reading delete

---
+++ Others_usagecat_update_script.sql
@@ -0,0 +1,10 @@
+-- Meter reading to delete for OTHERS usageCategory logged from 9-SEP-2021
+delete from eg_ws_meterreading where
+id not in (select distinct on (connectionno) id from eg_ws_meterreading order by connectionno, currentreadingdate)
+and connectionno in (select distinct connectionno from eg_ws_connection where id in (
+select connection_id from eg_ws_service where usagecategory='OTHERS')
+and isoldapplication=false and applicationstatus='CONNECTION_ACTIVATED')
+and date(to_timestamp(currentreadingdate/1000)) >= to_date('2021-09-09', 'yyyy-MM-dd');
+
+-- Update OTHERS to BPL
+update eg_ws_service set usagecategory='BPL' where usagecategory='OTHERS';
